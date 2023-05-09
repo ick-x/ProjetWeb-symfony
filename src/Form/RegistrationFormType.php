b01f2e6c8e545bff6f9faf921ed7fef714bd27b6
roms-grlt <romaiongroultlgc@gmail.com>
2023-05-09 21:47:08 +0200

    beaucoup de choses ;)

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -10,6 +10,7 @@ use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Email;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
@@ -24,6 +25,9 @@ class RegistrationFormType extends AbstractType
                 'required' => true,
                 'first_options'  => ['label' => 'Email'],
                 'second_options' => ['label' => 'Confirm Email'],
+                'constraints'=>[
+                    new Email(['message'=>'Please enter a valid email'])
+                ]
 
             ])
             ->add('name')
